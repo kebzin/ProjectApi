b4f9a04f0bf6a8fb5a8e203888a240c20ce9b169
kebzin <honorablewaiga@gmail.com>
2023-01-25 18:18:02 +0000

    commitpush

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./server'); // requesting the server file
-const PORT = 3009; // listing the port number
+const PORT = 3003; // listing the port number
 app.listen(PORT, () => {
     console.log(`Rest API listening on port ${PORT}`); // telling the user the pp is running on the port
 })
--- src/modules/connect.js
+++ src/modules/connect.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose'); // importing mongoose
-const URI = "mongodb+srv://kebba:Howareyoudoing1@rentalapp.9p5ry7s.mongodb.net/?retryWrites=true&w=majority" // link to my database at atlass
+// const URI = "mongodb+srv://kebba:Howareyoudoing1@cluster0.odehr6g.mongodb.net/?retryWrites=true&w=majority" // link to my database at atlass
 const connect = () => { // function that connect my app to the server
     return mongoose.connect(URI, { // inside mongoose there is a function calle connect which take a paramater, in the paramater am passing the URI
         useNewUrlParser: true, // no idea for now
